1ea015a5b1203c54e9810d802df15f70d557a4a2
Híu <hieunguyen12902@gmail.com>
2023-06-27 09:22:08 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import BaseLayout from '@/layouts/BaseLayout'
+import { ListData } from '@/utils/mockData'
+import { Col, Divider, List, Row } from 'antd'
+// import classes from './style.module.scss'
+
+export default function Home() {
+	return (
+		<BaseLayout>
+			<Row gutter={16}>
+				<Col className="gutter-row" span={12}>
+					<Divider orientation="left">Default Size</Divider>
+					<List
+						header={<div>Header</div>}
+						footer={<div>Footer</div>}
+						bordered
+						dataSource={ListData}
+						renderItem={(item) => <List.Item>{item}</List.Item>}
+					/>
+				</Col>
+			</Row>
+		</BaseLayout>
+	)
+}
