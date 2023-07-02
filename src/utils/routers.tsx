c1ed45d4ee1d1a3587e93309c8dcfdef4def3708
Híu <hieunguyen12902@gmail.com>
2023-07-02 15:03:41 +0700

    [FIX]: Api route

--- src/utils/routers.tsx
+++ src/utils/routers.tsx
@@ -1,19 +1,20 @@
+const BASE_URL = 'https://nextjs-sourcebase-json-server.vercel.app/' 
 // user routes
 export const APP_ROUTES = {
 	// homepage
-	HOME: '/',
+	HOME: `${BASE_URL}/`,
 
 	// authentication
-	LOGIN: '/login',
-	SIGNUP: '/signup',
+	LOGIN: `${BASE_URL}/login`,
+	SIGNUP: `${BASE_URL}/signup`,
 
 	// blogs
-	BLOGS: '/blogs',
+	BLOGS: `${BASE_URL}/blogs`,
 }
 
 export const ADMIN_ROUTES = {
-	DASHBOARD: '/dashboard',
+	DASHBOARD: `${BASE_URL}/dashboard`,
 
 	// blogs
-	CREATE: '/dashboard/create',
+	CREATE: `${BASE_URL}/dashboard/create`,
 }
