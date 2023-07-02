f648fad693294584e52d725999e620483ed8eb18
Híu <hieunguyen12902@gmail.com>
2023-07-02 20:05:06 +0700

    [FIX]: API route

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
 	images: {
 		domains: ['scontent.fsgn2-8.fna.fbcdn.net'],
 	},
-	distDir: "_next",
-	generateBuildId: async () => { if (process.env.BUILD_ID) { return process.env.BUILD_ID; } else { return `${new Date().getTime()}`; } },
 }
 
 module.exports = nextConfig
